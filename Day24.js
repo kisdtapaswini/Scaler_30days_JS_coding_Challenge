47fb1522d706038041a68a7664a21ec5921a2200
Tapaswini Swain <kisd.tapaswini@gmail.com>
2023-12-01 17:54:32 +0530

    Day 24

---
+++ Day24.js
@@ -0,0 +1,28 @@
+/**
+ * **Challenge 24**  
+Description:  
+Implement a debounce function that takes a function and a delay as arguments. The returned function should ensure that the original function is not called more than once in the specified delay.  
+Solution Approach:  
+Use a timer and clear it on each invocation to delay the function execution.
+ */
+
+function debounce(mainFun, delay){
+    let timer;
+    return function(...args){
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            mainFun(...args);
+        }, delay)
+    }
+}
+
+function saveInput(){
+    console.log('Saving data');
+}
+
+const processChange = debounce(() => saveInput(), 0);
+
+for(let i = 0; i<= 100000 ; i++){
+    processChange();
+}
